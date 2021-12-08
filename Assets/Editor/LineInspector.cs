a6f32a5fcd7aa2957e47d6edd5460a2b054e36e0
Thomas Lamb <thomaslambprogramming@gmail.com>
2021-12-08 23:25:18 +1100

    Added handle + undo options to line

--- Assets/Editor/LineInspector.cs
+++ Assets/Editor/LineInspector.cs
@@ -3,25 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+//This overwrites the default editor/inspector instead of the default unity inspector
 [CustomEditor(typeof(Line))]
 public class LineInspector : Editor
 {
     private void OnSceneGUI()
     {
+        //Editor class has a target variable which is set to the object to be drawn when onscenegui is called
+        //Since we know it will overwrite as the custom editor for the line we will have a target line.
         Line line = target as Line;
 
         Transform handleTransform = line.transform;
 
-        //is the tools pivot rotation local if true then give handletransform otherwise give default
-        Quaternion handleRotation = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
+        //This is the transforms rotation, I think the Tools.pivotRotation is a global unity setting if its true then we
+        //want to see where the handles are facing otherwise have them be a default (kinda cool)
+        Quaternion handleRotation = Tools.pivotRotation == 
+            PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
 
+        //We want the handles to be converted from local space (offset from position) to world space to be displayed correctly
         Vector3 p0 = handleTransform.TransformPoint(line.p0);
         Vector3 p1 = handleTransform.TransformPoint(line.p1);
 
         Handles.color = Color.white;
-        Handles.DrawLine(p0, p1);
-        Handles.DoPositionHandle(p0, handleRotation);
-        Handles.DoPositionHandle(p1, handleRotation);
 
+        //Seems like it checks to see if any editor value changes, draws the handles for each point, then if they did change 
+        //get the world position of the point (because it could be dragged) then convert back to the offset position (local position)
+        //this runs whenever scene is on, it does the world conversion otherwise it will fuck up and change everything
+        
+        EditorGUI.BeginChangeCheck();
+        p0 = Handles.DoPositionHandle(p0, handleRotation);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(line, "Move Point");
+            EditorUtility.SetDirty(line);
+            line.p0 = handleTransform.InverseTransformPoint(p0);
+        }
+
+        EditorGUI.BeginChangeCheck();
+        p1 = Handles.DoPositionHandle(p1, handleRotation);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(line, "Move Point");
+            EditorUtility.SetDirty(line);
+            line.p1 = handleTransform.InverseTransformPoint(p1);
+        }
+        
+        Handles.DrawLine(p0, p1);
     }
 }
--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class Line : MonoBehaviour
 {
+    //Point one and point 2 
     public Vector3 p0, p1;
+    
+
+    
 }
