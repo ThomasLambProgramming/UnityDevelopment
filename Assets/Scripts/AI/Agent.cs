5a90e2e64cbec25fa0998e1c1a4b632773f7de23
Thomas Lamb <thomaslambprogramming@gmail.com>
2022-01-16 23:53:29 +1100

    Ai ready for steering behaviours (mk4)

--- Assets/Scripts/AI/Agent.cs
+++ Assets/Scripts/AI/Agent.cs
@@ -29,12 +29,27 @@ public class Agent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (!_enableMovement || _target == Vector3.zero)
             return;
 
-        Vector3 desiredVelocity = Vector3.zero;
+        //Get the desiredVel from the current steering behaviour
+        Vector3 desiredVelocity = GetDesiredVelocity();
         
+        if (desiredVelocity.sqrMagnitude > (_maxTurningSpeed * _maxTurningSpeed))
+        {
+            desiredVelocity.Normalize();
+            desiredVelocity *= _maxTurningSpeed;
+        }
+
+        _agentRb.AddForce(desiredVelocity, ForceMode.VelocityChange);
+
+        if (_agentRb.velocity.sqrMagnitude > _maxSpeed * _maxSpeed)
+        {
+            Vector3 newVel = _agentRb.velocity.normalized;
+            newVel *= _maxSpeed;
+            _agentRb.velocity = newVel;
+        }
+
     }
     private Vector3 GetDesiredVelocity()
     {
@@ -54,7 +69,7 @@ public class Agent : MonoBehaviour
 
             case SteeringState.Pursuit:
                 return _steeringBehaviour.Pursue();
-                
+
             case SteeringState.Evade:
                 return _steeringBehaviour.Evade();
         }
@@ -66,6 +81,7 @@ public class Agent : MonoBehaviour
     public void SeekPosition(Vector3 a_position)
     {
         _target = a_position;
+        _steeringBehaviour.SetTargetLocation(a_position);
         _enableMovement = true;
     }
 }
--- Assets/Scripts/AI/SteeringBehaviours.cs
+++ Assets/Scripts/AI/SteeringBehaviours.cs
@@ -7,9 +7,11 @@ public class SteeringBehaviours
     
     private Rigidbody _agentRb = null;
     private Rigidbody _targetRb = null;
+    private Vector3 _targetPosition = Vector3.zero;
 
     public void SetAgentRb(Rigidbody a_rigid) => _agentRb = a_rigid;
     public void SetTargetRb(Rigidbody a_rigid) => _targetRb = a_rigid;
+    public void SetTargetLocation(Vector3 a_pos) => _targetPosition = a_pos;
 
     public float _moveSpeed = 0;
     public Vector3 Seek()
